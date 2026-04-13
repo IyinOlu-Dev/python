de5d31dcea89531996cf40f8f1b7115ec2e18852
IyinOlu-Dev <olutomiwa.oyegbola@gmail.com>
2026-04-13 10:26:58 -0500

    removed duplicate folders in repo

--- Day 1/Variable Manipulation.py
+++
@@ -1,6 +0,0 @@
-#Declaring variable
-Name  = (input("what is your name: \n"))
-BirthPlace =(input("where were you born:\n")).lower()
-
-print(f"Your band name could be {Name + BirthPlace}")
-print(BirthPlace)
